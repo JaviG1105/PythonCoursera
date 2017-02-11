a06a2e7661b40a4c595da1f7742c26ffc2b7378c
JaviG1105 <javi.garcia17@gmail.com>
2017-02-11 01:51:06 -0600

    adding first Assignment file

---
+++ firstAssignment.py
@@ -0,0 +1,11 @@
+import re
+
+count=0
+file = open('FirstAssignment.txt')
+for line in file:
+    line.rstrip()
+    list_file = re.findall('([0-9]+)',line)
+
+    for x in list_file:
+        count += int(x)
+print count
