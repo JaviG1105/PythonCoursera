05c64be6196a3b6f4e993f62a0456916d33d04cd
JaviG1105 <javi.garcia17@gmail.com>
2017-02-18 23:03:33 -0600

    changed urllib command to work for Mac

--- Assignments/fourthAssignment.py
+++ Assignments/fourthAssignment.py
@@ -1,9 +1,9 @@
 import xml.etree.ElementTree as ET
-import urllib.request
+import urllib
 
 xmlURL = 'http://python-data.dr-chuck.net/comments_353496.xml'
 
-uh = urllib.request.urlopen(xmlURL)
+uh = urllib.urlopen(xmlURL)
 data = uh.read()
 tree = ET.fromstring(data)
 
@@ -12,4 +12,4 @@ sum = 0
 for item in list:
     sum += int(item.text)
 
-print (sum)
+print sum
